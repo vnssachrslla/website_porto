6272dea17ed4384d3d790284f13345509d58361b
vnssachrslla <vanessa.chrestella@gmail.com>
2020-09-22 14:52:02 +1000

    commit1

---
+++ functionality.js
@@ -0,0 +1,17 @@
+/* MODAL FUNCTIONALITY JAVASCRIPT */
+
+function openModal1() {
+    document.getElementById("sophiaModal").style.display = "block";
+}
+
+function openModal2() {
+    document.getElementById("annaModal").style.display = "block";
+}
+
+function openModal3() {
+    document.getElementById("jacquelineModal").style.display = "block";
+}
+
+function openModal4() {
+    document.getElementById("emilyModal").style.display = "block";
+}
